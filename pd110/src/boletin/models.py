806c7bb2316cc9ee38b744b0c66bf19c4c0476a6
antoniomule <43681770+antoniomule@users.noreply.github.com>
2019-05-13 01:16:56 +0200

    Commit 5
    Video6. Primero modelo, he tenido problemas al migrar, he tenido que dejar comentados las lineas del model.py "def....."

--- pd110/src/boletin/models.py
+++ pd110/src/boletin/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Registrado(models.Model):
+   nombre = models.CharField(max_length=100, blank=True, null=True)
+   email = models.EmailField(max_length=254, default='ejemplo@correo.com')
+   timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, default='ddmmyy')
+ 
+    #def _str_(self)
+        #return self.email
