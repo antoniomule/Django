4a86fdcc3a0cd8fe0c02a0c0e12ff758f40da3c4
antoniomule <43681770+antoniomule@users.noreply.github.com>
2019-05-13 02:09:31 +0200

    Commit 6 / Video7
    Video 7, He cambiado la clase de modelo del video anterior, que en el anterior commit me daba fallos al migrar, pero ahora está 100% correcta

--- pd110/src/boletin/models.py
+++ pd110/src/boletin/models.py
@@ -4,9 +4,12 @@ from django.db import models
 
 # Create your models here.
 class Registrado(models.Model):
-   nombre = models.CharField(max_length=100, blank=True, null=True)
-   email = models.EmailField(max_length=254, default='ejemplo@correo.com')
-   timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, default='ddmmyy')
- 
-    #def _str_(self)
-        #return self.email
+	nombre = models.CharField(max_length=100, blank=True, null=True)
+	email = models.EmailField()
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+	def __unicode__(self):
+		return self.email
+
+	def __str__(self):
+		return self.email
