09b9270b17732cfb888a7ae15bf602b96c55b953
antoniomule <43681770+antoniomule@users.noreply.github.com>
2019-05-14 14:03:46 +0200

    Commit 15 / Video16

--- pd110/src/boletin/forms.py
+++ pd110/src/boletin/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 
+from .models import Registrado
+
+class RegModelForm(forms.ModelForm):
+	class Meta:
+		model = Registrado
+		fields = ["nombre", "email"]
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(required=False)
 	email = forms.EmailField()
