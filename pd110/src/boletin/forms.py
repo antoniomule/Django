00149b52ee9ba43cd7f0c96055695390028dcb73
antoniomule <43681770+antoniomule@users.noreply.github.com>
2019-05-15 14:17:21 +0200

    Commit 16 / Video17
    Validando los registros

--- pd110/src/boletin/forms.py
+++ pd110/src/boletin/forms.py
@@ -7,6 +7,20 @@ class RegModelForm(forms.ModelForm):
 		model = Registrado
 		fields = ["nombre", "email"]
 
+	def clean_email(self):
+		email = self.cleaned_data.get("email")
+		email_base, proveeder = email.split("@")
+		dominio, extension = proveeder.split(".")
+		if not extension == "edu":
+			raise forms.ValidationError(" Porfi primo utiliza una extension .edu")
+		return email
+
+		def cleam_nomber(self):
+			nombre = self.cleaned_data.get("nombre")
+			#validaciones
+			return nombre
+
+
 class RegForm(forms.Form):
 	nombre = forms.CharField(required=False)
 	email = forms.EmailField()
