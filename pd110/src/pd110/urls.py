e5d72e1c90274971dde1fc2367022ab49097219e
antoniomule <43681770+antoniomule@users.noreply.github.com>
2019-05-28 02:55:34 +0200

    Commit 30

--- pd110/src/pd110/urls.py
+++ pd110/src/pd110/urls.py
@@ -1,5 +1,4 @@
 """pd110 URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
 Examples:
@@ -13,22 +12,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
 from boletin import views
 from .views import about
-# from boletin.views inicio
+# from boletin.views import inicio
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^contact/$', views.contact, name='contact'),
     url(r'^$', views.inicio, name='inicio'),
-    url(r'^about/$', about, name='about')
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^about/$', about, name='about'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-   # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
